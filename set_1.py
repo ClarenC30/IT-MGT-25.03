e1125a9d878f716bc6cbc75b256f4a87d900e5f1
ClarenC30 <clarence.caballero@student.ateneo.ede>
2025-06-19 22:10:12 +0800

    Module 2

--- set_1.py
+++ set_1.py
@@ -1,23 +1,122 @@
-def savings(gross_pay, tax_rate, expenses):
-    print(gross_pay - (gross_pay * tax_rate) - expenses)
+def interest(principal, rate, periods):
+    '''Interest.
+
+    This function calculates the final value of an investment after
+        gaining simple interest over a number of periods.
+
+    To calculate simple interest, simply multiply the principal to the quantity (rate * time).
+        Add this amount to the principal to get the final value.
+
+    Round down the final amount.
+
+    Parameters
+    ----------
+    principal: int
+        the principal (i.e., starting) amount invested, expressed in centavos
+    rate: float
+        the interest rate per period, expressed as a decimal representation of a percentage (e.g., 3% is 0.03)
+    periods: int
+        the number of periods invested
+
+    Returns
+    -------
+    int
+        the final value of the investment
+    '''
+    # Replace `pass` with your code.
+    # Stay within the function. Only use the parameters as input. The function should return your answer.
+    pass
+
+  print(gross_pay - (gross_pay * tax_rate) - expenses)
+
+
 
 savings(1000, 0, 500)
 500
 
 
+
+
+
 def material_waste(total_material, material_units, num_jobs, job_consumption):
+    '''Material Waste.
+
+    This function calculates how much material input will be wasted
+        after running a certain number of jobs that consume
+        a set amount of material.
+
+    To get the waste of a set of jobs:
+        1. Multiply the number of jobs by the material consumption per job.
+        2. Subtract the total material consumed from the total material available.
+
+    The users of this function also want you to format the output as a string, annotated with the
+        units in which the material is expressed. Do not add a space between the number and the unit.
+
+    Parameters
+    ----------
+    total_material: int
+        the total material available
+    material_units: str
+        the units used to express a quantity of the material (e.g., "kg", "L", etc.)
+    num_jobs: int
+        the number of jobs to run
+    job_consumption: int
+        the amount of material consumed per job
+
+    Returns
+    -------
+    str
+        the amount of remaining material expressed with its unit (e.g., "10kg").
+    '''
+    # Replace `pass` with your code.
+    # Stay within the function. Only use the parameters as input. The function should return your answer.
+
     value = total_material - (num_jobs * job_consumption)
     unit = material_units
     result = str(value) + unit
     print(result)
 
+
+
 material_waste(1000,"kg",3,100)
 700kg
 
 
+
+
+
 def interest(principal, rate, periods):
+    '''Interest.
+
+    This function calculates the final value of an investment after
+        gaining simple interest over a number of periods.
+
+    To calculate simple interest, simply multiply the principal to the quantity (rate * time).
+        Add this amount to the principal to get the final value.
+
+    Round down the final amount.
+
+    Parameters
+    ----------
+    principal: int
+        the principal (i.e., starting) amount invested, expressed in centavos
+    rate: float
+        the interest rate per period, expressed as a decimal representation of a percentage (e.g., 3% is 0.03)
+    periods: int
+        the number of periods invested
+
+    Returns
+    -------
+    int
+        the final value of the investment
+    '''
+    # Replace `pass` with your code.
+    # Stay within the function. Only use the parameters as input. The function should return your answer.
+    
     final_value = principal + (principal * (rate * periods))
     print(final_value)
+
+
 
 interest(1000,0.06,8)
 1480.0
